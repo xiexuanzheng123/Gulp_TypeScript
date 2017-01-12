d975e5b5dca5aab999a79f70b055e851d17e45bf
xiexuanzheng123 <349328984@qq.com>
2017-01-12 19:31:27 +0800

    use browser-sync to manage the code

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
+var browserSync = require('browser-sync').create();
 
 gulp.task('tsc',function(){
-    gulp.src('typescript/**.ts')
+    gulp.src('typescript/**/*.ts')
         .pipe(ts())
         .pipe(gulp.dest('javascript/'));
+});
+
+gulp.task('tsc:w',['tsc'],function(){
+    gulp.watch('typescript/**/*.ts',['tsc']);
 });
--- javascript/dataType.js
+++
@@ -1,4 +0,0 @@
-var myname = 'Gene';
-var age = 37;
-var sentence = "Hello, my name is " + myname + ". I'll be " + (age + 1) + " years old next year.";
-console.log(sentence);
