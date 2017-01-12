eb13f0cdd5fdcabba349460b120377f87932c072
xiexuanzheng123 <349328984@qq.com>
2017-01-12 19:34:57 +0800

    use browser-sync to manage the code

---
+++ javascript/dataType/dataType.js
@@ -0,0 +1,8 @@
+var myname = 'Gene';
+var age = 37;
+var sentence = "Hello, my name is " + myname + ". I'll be " + (age + 1) + " years old next year.";
+console.log(sentence);
+var list1 = [1, 2, 3];
+var list2 = [1, 2, 3];
+var x;
+x = ['Hello', 10];
---
+++ typescript/dataType/dataType.ts
@@ -0,0 +1,10 @@
+var myname: string = 'Gene';
+var age: number = 37;
+var sentence: string = `Hello, my name is ${ myname }. I'll be ${ age + 1 } years old next year.`
+
+console.log(sentence);
+var list1: number[] = [1, 2, 3];
+var list2: Array<number> = [1, 2, 3];
+
+var x: [string,number];
+x = ['Hello',10];
